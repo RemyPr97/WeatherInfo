f8b33ebc4c1dafd1787d58d07c07c130f9d39e43
Eno LEROY <eno.leroy@gmail.com>
2023-08-08 16:30:19 +0200

    cartes de villes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,16 @@
 class PagesController < ApplicationController
   def home
+    @meteo_test = {
+      paris: {
+        temp: '25°C',
+        weather: 'cloudy'
+      },
+      marseille: {
+        temp: '35°C',
+        weather: 'sunny'
+      }
+    }
+
   end
 
   def about
