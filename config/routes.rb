0f0e1ae189c6d74e2a15a96af672164e1f8a0049
Remy Prince <remyprince97@gmail.com>
2023-08-08 16:54:12 +0200

    add about et contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "pages#home"
+  # Defines the about path route ("/about")
+  get "/about", to: "pages#about"
+  # Defines the contact path route ("/contact")
+  get "/contact", to: "pages#contact"
 end
