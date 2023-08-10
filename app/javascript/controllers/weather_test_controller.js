c49f555aec6c4ca0761a6c6668d4bc5903025d6c
evans hulot <hulotevans@gmail.com>
2023-08-10 17:32:57 +0200

    weather api

--- app/javascript/controllers/weather_test_controller.js
+++ app/javascript/controllers/weather_test_controller.js
@@ -3,14 +3,26 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="weather-test"
 export default class extends Controller {
   static values = { weatherKey: String }
+  static targets = [ "cities" ]
+
+  displayCurrentConditions(data){
+    const card = `<div>
+                    <p>${data.resolvedAddress}</p>
+                    <p>${data.currentConditions.temp}°</p>
+                  </div>
+                  <p>${data.currentConditions.icon}</p>`
+    this.citiesTarget.insertAdjacentHTML("beforeend", card);
+  }
 
   connect() {
     const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/paris?unitGroup=metric&key=${this.weatherKeyValue}&contentType=json`
 
+    console.log(this.citiesTarget)
     fetch(url) // Make the HTTP request
       .then(response => response.json()) // Wait for the response and parse it as JSON
       .then((data) => {
         console.log(data)
+        this.displayCurrentConditions(data)
       })
   }
 }
